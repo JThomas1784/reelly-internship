5b5886c9c204ae81853fe873756e3ba71d5de996
Jamonte T <jamonte.thomas17@gmail.com>
2025-01-18 15:27:07 -0500

    Headless mode

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,31 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# def before_all(context):
+#     chrome_options = Options()
+#     chrome_options.add_argument("--headless")
+#     chrome_options.add_argument("--disable-gpu")
+#     chrome_options.add_argument("--window-size=1920,1080")
+#     chrome_options.add_argument("--no-sandbox")
+#     chrome_options.add_argument("--disable-dev-shm-usage")
+#
+#     context.driver = webdriver.Chrome(options=chrome_options)
+#     context.driver.maximize_window()
+#     print(f"Running in headless mode: {chrome_options.arguments}")
+#
+# def after_all(context):
+#     context.driver.quit()
+
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 def before_all(context):
-    context.driver = webdriver.Chrome()
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+    firefox_options.add_argument("--window-size=1920,1080")
+
+    context.driver = webdriver.Firefox(options=firefox_options)
     context.driver.maximize_window()
 
 def after_all(context):
